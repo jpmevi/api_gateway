79d85e92e94405f18c1794337ad615ade69688b9
juan.meza <juan.meza@bam.com.gt>
2024-10-26 20:03:05 -0600

    fixed dns

--- src/main/java/com/jpmevi/gateway/infrastructure/config/SecurityConfig.java
+++ src/main/java/com/jpmevi/gateway/infrastructure/config/SecurityConfig.java
@@ -1,10 +1,14 @@
 package com.jpmevi.gateway.infrastructure.config;
 
+import io.netty.resolver.DefaultAddressResolverGroup;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.netty.http.client.HttpClient;
 
 
 @Configuration
@@ -16,4 +20,15 @@ public class SecurityConfig {
         http.csrf(csrf -> csrf.disable()); // Disable CSRF
         return http.build();
     }
+
+    @Bean
+    public WebClient webClient() {
+        HttpClient httpClient = HttpClient.create()
+                .resolver(DefaultAddressResolverGroup.INSTANCE);
+
+        return WebClient.builder()
+                .clientConnector(new ReactorClientHttpConnector(httpClient))
+                .build();
+    }
+
 }
